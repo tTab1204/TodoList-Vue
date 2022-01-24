588bf6aecb7f7615815e8092fe992041a174a3cc
이주영 <ken44929@gmail.com>
2022-01-24 16:58:23 +0900

    feat: smacss rule 추가

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,9 @@
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
-  font-family: Pretendard;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  font-family: "Pretendard";
+  font-size: 12px;
 }
 </style>
